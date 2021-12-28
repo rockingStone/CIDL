2a288c6bde8abc67178cbba2f96f65ff5783d730
lightfindyou <3148438700@qq.com>
2021-12-28 17:05:00 +0800

    add experiment shell, output, post process script and final result

--- expriment/leveldb/inlineDestorDiffValueSize.sh
+++ expriment/leveldb/inlineDestorDiffValueSize.sh
@@ -3,29 +3,34 @@
 
 dbBash=/pmem/ 
 dedupPath=/pmem/dedupDir
-#valueSize=(128 256 512 1024 2048 4096 8192)
-valueSize=(1024)
+valueSize=(128 256 512 1024 2048 4096 8192)
+#valueSize=(128)
+#chunkMethod=("normalized rabin" "file" "fixed" "fastcdc" "tttd" "ae" "rabin")
+chunkMethod=("fixed" "tttd" "ae" "rabin" "fastcdc" )
+
 output=""
 for s in "${valueSize[@]}"
 do
 	#clean path
 	dbPath="${dbBash}db${s}/"
-	set -o xtrace
-	find $dbPath -type f -exec rm {} \;
 
-#	time /pmem/benchmark/db_bench_modified --benchmarks=fillrandom --value_size=$s --db=$dbPath
-	time LD_PRELOAD=/home/xzjin/Documents/destor/src/.libs/libentrance.so.0.0.0  /home/xzjin/leveldb/build/db_bench --benchmarks=fillrandom --value_size=$s --db=$dbPath
+	for c in "${chunkMethod[@]}"
+	do
+		find $dbPath -type f -exec rm {} \;
+		newline="chunk-algorithm $c"
+		gawk -i inplace "/^chunk-algorithm/{gsub(\"^chunk-algorithm[a-zA-Z ]*\" "," \"$newline\")};{print}" destor.config
+		set -o xtrace
+		time LD_PRELOAD=/home/xzjin/Documents/destor/src/.libs/libentrance.so.0.0.0  /home/xzjin/leveldb/build/db_bench --benchmarks=fillrandom --value_size=$s --db=$dbPath
+		du -sb $dbPath
+		set +o xtrace
+		echo; echo;
+	done
+
 	find $dbPath -type f -exec rm {} \;
+	set -o xtrace
 	time /home/xzjin/leveldb_modified/build/db_bench --benchmarks=fillrandom --value_size=$s --db=$dbPath
+	set +o xtrace
 	du -sb $dbPath
 #	time /usr/local/bin/destor $dbPath -p"chunk-algorithm fastcdc" 
-	set +o xtrace
 	echo; echo; 
 done
-#destor /home/xzjin/dbtmp -p"chunk-algorithm 'normalized rabin'"
-#destor /home/xzjin/dbtmp -p"chunk-algorithm file"
-#destor /home/xzjin/dbtmp -p"chunk-algorithm fixed"
-#destor /home/xzjin/dbtmp -p"chunk-algorithm tttd"
-#destor /home/xzjin/dbtmp -p"chunk-algorithm ae"
-#destor /home/xzjin/dbtmp -p"chunk-algorithm rabin"
-#destor /pmem/dbtmp -p"chunk-algorithm fastcdc"
