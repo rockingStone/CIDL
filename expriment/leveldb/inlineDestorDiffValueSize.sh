e88c21850afff85ed991919d0d340a17dc7860b7
lightfindyou <3148438700@qq.com>
2022-01-03 16:20:28 +0800

    add 4K,32K block size script and data

--- expriment/leveldb/inlineDestorDiffValueSize.sh
+++ expriment/leveldb/inlineDestorDiffValueSize.sh
@@ -7,6 +7,8 @@ valueSize=(128 256 512 1024 2048 4096 8192)
 #valueSize=(128)
 #chunkMethod=("normalized rabin" "file" "fixed" "fastcdc" "tttd" "ae" "rabin")
 chunkMethod=("fixed" "tttd" "ae" "rabin" "fastcdc" )
+#blockSize="4096"
+blockSize="32768"
 
 output=""
 for s in "${valueSize[@]}"
@@ -20,7 +22,7 @@ do
 		newline="chunk-algorithm $c"
 		gawk -i inplace "/^chunk-algorithm/{gsub(\"^chunk-algorithm[a-zA-Z ]*\" "," \"$newline\")};{print}" destor.config
 		set -o xtrace
-		time LD_PRELOAD=/home/xzjin/Documents/destor/src/.libs/libentrance.so.0.0.0  /home/xzjin/leveldb/build/db_bench --benchmarks=fillrandom --value_size=$s --db=$dbPath
+		time LD_PRELOAD=/home/xzjin/Documents/destor/src/.libs/libentrance.so.0.0.0  /home/xzjin/leveldb/build/db_bench --benchmarks=fillrandom --value_size=$s --db=$dbPath --block_size=$blockSize 
 		du -sb $dbPath
 		set +o xtrace
 		echo; echo;
@@ -28,7 +30,7 @@ do
 
 	find $dbPath -type f -exec rm {} \;
 	set -o xtrace
-	time /home/xzjin/leveldb_modified/build/db_bench --benchmarks=fillrandom --value_size=$s --db=$dbPath
+	time /home/xzjin/leveldb_modified/build/db_bench --benchmarks=fillrandom --value_size=$s --db=$dbPath --block_size=$blockSize 
 	set +o xtrace
 	du -sb $dbPath
 #	time /usr/local/bin/destor $dbPath -p"chunk-algorithm fastcdc" 
--- expriment/leveldb/processData.sh
+++ expriment/leveldb/processData.sh
@@ -1,42 +1,55 @@
 #!/bin/bash
 
+#stop on error
+set -e
+
 category=(128 256 512 1024 2048 4096 8192)
 column=("FSC" "TTTD" "AE" "RABIN" "FastCDC" "CIDL")
-#use array?
-#lineNum="$(grep -n "needle" haystack.txt | head -n 1 | cut -d: -f1)"
+#For 4K
+startLineNum=(1 229 457 687 923 1175 1467)
+endLineNum=(226 455 685 921 1173 1465 1841)
+output=4K.CSV
+fileName="inlineDestorDiffValueSize.sh_log_4K_fullIndex"
 
-for a in "${category[@]}"
+#For 32K
+#startLineNum=(1 228 456 685 915 1145 1375)
+#endLineNum=(227 455 684 914 1144 1374 1604)
+#output=32K.CSV
+#fileName="inlineDestorDiffValueSize.sh_log_32K_fullIndex"
+
+mkdir -p ./tmp/
+
+for a in "${!category[@]}"
 do
-	echo -n > $a.csv
+	echo -n > ${category[a]}.csv
 	chunkMethods=" "
-	bigTitle=$a
+	bigTitle=${category[a]}
 	echo $bigTitle
 	for c in "${column[@]}"
 	do
 		chunkMethods=$chunkMethods","$c
 		bigTitle=$bigTitle","
 	done
-	echo $bigTitle >> $a.csv
-	echo $chunkMethods >> $a.csv
+	echo $bigTitle >> ${category[a]}.csv
+	echo $chunkMethods >> ${category[a]}.csv
 	
 	set -o xtrace
-	fileName="inlineDestorDiffValueSize.sh_log_4K_fullIndex"
-	awk "{if(FNR>=1 && FNR<226) {printf(\"%s\n\", \$0)} }" $fileName > tmp/filteredData
+	awk "{if(FNR>=${startLineNum[a]} && FNR<${endLineNum[a]}) {printf(\"%s\n\", \$0)} }" $fileName > tmp/filteredData
 	grep -e "new metadata item" -e "hash number" -e "--value_size=" -e "MB/s" -e "real" -e "ts_write same bytes" -e "duplicate percentage" -e "chunck algorithm" tmp/filteredData > tmp/grepedData
 	
 	#Throughput
-	awk "BEGIN{line=\"throuthput\"} /^fillrandom/{line=line\",\"\$(NF-1) } END{print line}" tmp/grepedData >> $a.csv
+	awk "BEGIN{line=\"throuthput\"} /^fillrandom/{line=line\",\"\$(NF-1) } END{print line}" tmp/grepedData >> ${category[a]}.csv
 	
 	#DedupRatio
 	awk "/duplicate percentage/{print \$(NF)} /ts_write same bytes/{print \$(NF)}" tmp/grepedData > tmp/DedepRatio
-	awk -F"%" "BEGIN{line=\"dedupRatio\"} {line=line\",\"\$1 } END{print line}" tmp/DedepRatio >> $a.csv
+	awk -F"%" "BEGIN{line=\"dedupRatio\"} {line=line\",\"\$1 } END{print line}" tmp/DedepRatio >> ${category[a]}.csv
 	
 	#Time
 	awk "/real/{print \$(NF)}" tmp/grepedData > tmp/Time
-	awk -F"[ms]" "BEGIN{line=\"time\"} {time=\$1*60+\$2; line=line\",\"time} END{print line}" tmp/Time >> $a.csv
+	awk -F"[ms]" "BEGIN{line=\"time\"} {time=\$1*60+\$2; line=line\",\"time} END{print line}" tmp/Time >> ${category[a]}.csv
 	
 	#hash/meta num
-	awk "BEGIN{line=\"hash/meta num\"} /hash number/{line=line\",\"\$(NF)} /new metadata item/{metaNum=substr(\$7, 2, length(\$7)-2); line=line\",\"metaNum} END{print line}" tmp/grepedData >> $a.csv
+	awk "BEGIN{line=\"hash/meta num\"} /hash number/{line=line\",\"\$(NF)} /new metadata item/{metaNum=substr(\$7, 2, length(\$7)-2); line=line\",\"metaNum} END{print line}" tmp/grepedData >> ${category[a]}.csv
 
 done
 
@@ -45,4 +58,4 @@ for a in "${category[@]}"
 do
 	mergePara=$mergePara$a".csv "
 done
-paste -d"," $mergePara > 4K.csv
+paste -d"," $mergePara > $output
