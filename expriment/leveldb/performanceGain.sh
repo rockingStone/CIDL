67a7507c80ac40151a5f46b9698226b1b644ef03
lightfindyou <3148438700@qq.com>
2022-01-06 15:27:23 +0800

    add change performance gain to less compile, add data process srcipt

--- expriment/leveldb/performanceGain.sh
+++ expriment/leveldb/performanceGain.sh
@@ -1,11 +1,8 @@
 #! /bin/bash
-# --num=5000000
 
 dbBash=/pmem/ 
-valueSize=(128 256 512 1024 2048 4096 8192)
-#valueSize=(128)
-#chunkMethod=("normalized rabin" "file" "fixed" "fastcdc" "tttd" "ae" "rabin")
-chunkMethod=("fixed" "tttd" "ae" "rabin" "fastcdc" )
+#valueSize=(128 256 512 1024 2048 4096 8192)
+valueSize=(1024)
 blockSize="4096"
 #blockSize="32768"
 
@@ -18,6 +15,7 @@ function runDB(){
 	
 	#	find $dbPath -type f -exec rm {} \;
 		set -o xtrace
+#		time LD_PRELOAD=/home/xzjin/awn/libawn.so /home/xzjin/leveldb_modified/build/db_bench --benchmarks=fillrandom --value_size=$s --db=$dbPath --block_size=$blockSize 
 		time /home/xzjin/leveldb_modified/build/db_bench --benchmarks=fillrandom --value_size=$s --db=$dbPath --block_size=$blockSize 
 		set +o xtrace
 		du -sb $dbPath
@@ -25,8 +23,33 @@ function runDB(){
 	done
 }
 
-cd /home/xzjin/awn/
-make base
-sudo make install
-echo "base version"
-runDB
+function base(){
+	cd /home/xzjin/awn/
+	make base
+	sudo make install
+	cd /home/xzjin/awn/expriment/leveldb
+	echo "base version"
+	runDB
+}
+
+function AVX(){
+	cd /home/xzjin/awn/
+	make pg_threelevel_delNode
+	sudo make install
+	cd /home/xzjin/awn/expriment/leveldb
+	echo "disable AVX-2"
+	runDB
+}
+
+function normal(){
+	cd /home/xzjin/awn/
+	make 
+	sudo make install
+	cd /home/xzjin/awn/expriment/leveldb
+	echo "disable AVX-2"
+	runDB
+}
+
+base
+AVX
+normal
